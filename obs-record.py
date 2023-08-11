bd5ac1427635175f9eaa5ab3400fa0f698003303
AlperHuseyn <alperdogan@hotmail.com>
2023-08-11 20:44:54 +0300

    Add is_player_ended to stop OBS recording and other improvments

--- obs-record.py
+++ obs-record.py
@@ -9,18 +9,24 @@ tailored to your requirements.
 Author: Alper Huseyin DOGAN
 """
 
+import colorama
+from colorama import Back
+colorama.init(autoreset=True)
 from obswebsocket import obsws, requests
 from selenium import webdriver 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from time import sleep
 
+        
 class ObsRecorder():
-    def __init__(self):
-        self.driver = webdriver.Edge()  # Default web browser is Edge in my case, change if necessary
+    def __init__(self, host, port, password):
+        self.driver = webdriver.Edge()  # choose a browser
         self.wait = WebDriverWait((self.driver), 10)
+        self.host = host
+        self.port = port
+        self.password= password
         
     def open_website(self, url):
         self.driver.get(url)
@@ -57,39 +63,57 @@ class ObsRecorder():
         
         # Switch to the newly opened tab
         self.driver.switch_to.window(self.driver.window_handles[-1])
-        
-    
+            
     def set_video_speed(self, speed):
         # Execute JavaScript to change video playback speed
         self.driver.execute_script(f'document.querySelector("video").playbackRate = {speed}')
+        # Get the current playback speed and check if it's set correctly
+        curr_speed = self.driver.execute_script('return document.querySelector("video").playbackRate')
+        
+        if curr_speed == speed:
+            print(Back.GREEN + f'Video speed set to x{speed} successfully.' + '\033[39m')
+        else:
+            print(Back.RED + 'Video speed was not set correctly.' + '\033[39m')
+    
+    def get_current_video_id(self):
+        current_url = self.driver.current_url
+        return current_url[current_url.rfind('/') + 1:]
     
     def expand_video_to_fullscreen(self):
-        current_url = self.driver.current_url
-        id = current_url[current_url.rfind('/') + 1:]
+        id = self.get_current_video_id()
         video_elem = self.driver.find_element(By.XPATH, f'//*[@id="{id}"]/div[4]')
+        
+        # Get the initial size of the video element
+        initial_size = video_elem.size
+        
+        # Double click to expand the window
         actions = ActionChains((self.driver))
         actions.double_click(video_elem).perform()
         
-    def open_OBS_studio_and_record(self):
-        host = 'xx.xxx.xx.x'  # your host name
-        port = 4455
-        password = 'xxxxxxxxxxxxxxxx'  # your password for OBS connection
+        # Get the initial size of the video element
+        updated_size = video_elem.size
         
+        if updated_size != initial_size:
+            print(Back.GREEN + 'Video maximized successfully.' + '\033[39m')
+        else:
+            print(Back.GREEN + 'Video was not maximized correctly.' + '\033[39m')
+            
+    def open_OBS_studio_and_record(self):
         try:
             print("Connecting to OBS WebSocket server...")
-            obs_ws = obsws(host, port, password)
+            obs_ws = obsws(host=self.host, port=self.port, password=self.password)
             obs_ws.connect()
             
-            print("Connected to OBS WebSocket server.")
+            print(Back.GREEN + "Connected to OBS WebSocket server." + '\033[39m')
             
             print("Sending request to start recording...")
             response = obs_ws.call(requests.StartRecord())
             if response.status:    
-                print("Recording started successfully. Response: ", response.status)
+                print(Back.GREEN + "Recording started successfully" + '\033[39m' + ". Response:", Back.GREEN + str(response.status) + '\033[39m')
                 print("Waiting for video to finish...")
-                sleep(10)  # Change here, it should be the video lenght in seconds.
+                
             else:
-                print("Failed to start recording. Response: ", response.status)
+                print(Back.RED + "Failed to start recording. Response:", str(response.status) + '\033[39m')
             print("Disconnecting from OBS WebSocket server...")
             obs_ws.disconnect()
             print("Disconnected from OBS WebSocket server.")
@@ -97,52 +121,59 @@ class ObsRecorder():
             print("Failed to start recording:", str(e))            
             
     def close_OBS_studio_and_stop_recording(self):
-        host = 'xx.xxx.xx.x'  # your host name
-        port = 4455
-        password = 'xxxxxxxxxxxxxxxx'  # your password for OBS connection
-        
         try:
             print("Connecting to OBS WebSocket server...")
-            obs_ws = obsws(host, port, password)
+            obs_ws = obsws(host=self.host, port=self.port, password=self.password)
             obs_ws.connect()
             
-            print("Connected to OBS WebSocket server.")
+            print(Back.GREEN + "Connected to OBS WebSocket server." + '\033[39m')
     
             print("Sending request to stop recording...")
             response = obs_ws.call(requests.StopRecord())
             if response.status:
-                print("Recording stopped successfully. Response: ", response.status)
+                print(Back.GREEN + "Recording stopped successfully" + '\033[39m' + ". Response:", Back.GREEN + str(response.status) + '\033[39m')
             else:
-                print("Failed to stop recording Response: ", response.status)
+                print(Back.RED + "Failed to stop recording. Response:", str(response.status) + '\033[39m')
     
             print("Disconnecting from OBS WebSocket server...")
             obs_ws.disconnect()
             print("Disconnected from OBS WebSocket server.")
         except Exception as e:
             print("Failed to stop recording: ", str(e))
-
-def main():    
-    start_video_num = 61
-    end_video_num = 61
     
-    for video_num in range(start_video_num, end_video_num - 1,  -1):
-        obs = ObsRecorder()
-        obs.open_website(r'https://example.com/')  # The website contains the video
-        obs.login('example@xyz.com', 'xxxxxxxx')  # Login info    
-        obs.click_elem((By.XPATH, '/html/body/header/nav/div/div/ul[2]/li[2]/a'))
-        obs.click_elem((By.XPATH, '/html/body/div/main/table/tbody/tr[2]/td[1]/a'))    
-        
+    def is_player_ended(self):
+        return self.driver.execute_script("""
+            var video = document.querySelector("video");
+            return video.ended || (video.currentTime >= video.duration);
+        """)
+
+def main():        
+    start_video_num = 49  # Change here
+    end_video_num = 39  # change here
+    for course_num, video_num in enumerate(range(start_video_num, end_video_num - 1,  -1), start=48):
+        obs = ObsRecorder(host='xxx.xxx.x.xx', port=4455, password='xxxxxxxxxxxxxxxx')    
+        obs.open_website(r'https://example.org/')  # The website contains the video        
+        obs.login('example@xyz.com', 'xxxxxxxx')  # Login info            
+        obs.click_elem((By.XPATH, '/html/body/header/nav/div/div/ul[2]/li[2]/a'))        
+        obs.click_elem((By.XPATH, '/html/body/div/main/table/tbody/tr[2]/td[1]/a'))                
         password = obs.get_video_password(video_num)
         obs.open_video_link(video_num)    
         obs.enter_text((By.XPATH, '//*[@id="password"]'), password)
         obs.click_elem((By.XPATH, '//*[@id="pw_form"]/input[5]'))
-        obs.set_video_speed(2)
+        print('#-----------------------------------------#')
+        print(f'## ders {course_num} ##')
+        obs.set_video_speed(2)        
         obs.expand_video_to_fullscreen()
         obs.open_OBS_studio_and_record()
-        obs.close_OBS_studio_and_stop_recording()
+        
+        while True:
+            if obs.is_player_ended():
+                obs.close_OBS_studio_and_stop_recording()
+                break
         
         obs.driver.close()
-        
+    
+    print('#-----------------------------------------#')
     input('Press enter for closing the browser...')
 
 
