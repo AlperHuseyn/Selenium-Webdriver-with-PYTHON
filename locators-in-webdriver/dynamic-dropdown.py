08f7ed8f8875e53bdb3efb72ac4829ac5fcc7c93
AlperHuseyn <alperdogan@hotmail.com>
2023-08-17 15:39:42 +0300

    Create dynamic-dropdown.py file

--- locators-in-webdriver/dynamic-dropdown.py
+++ locators-in-webdriver/dynamic-dropdown.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from time import sleep
 
 
 WEBSITE_URL = r'https://rahulshettyacademy.com/dropdownsPractise/'
-EMAIL = 'your_email@example.com'
-PASSWORD = 'your_password_here'
-NAME = 'your_name_here'
 
 service = Service()
 driver = webdriver.Chrome(service=service)
 
 driver.get(WEBSITE_URL)
+
+driver.find_element(By.ID, 'autosuggest').send_keys('ind')
+sleep(2)  # Add delay to see suggestions
+countries = driver.find_elements(By.CSS_SELECTOR, 'li[class="ui-menu-item"] a')
+
+for country in countries:
+    if country.text == 'India':
+        country.click()
+        break
+        
+assert driver.find_element(By.ID, 'autosuggest').get_attribute('value') == 'India'
+
+# Close driver
+driver.close()
+
+
+
