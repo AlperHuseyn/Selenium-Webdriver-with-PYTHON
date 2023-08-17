ac7df430ae69e297aba703a49af9fea5642936c4
AlperHuseyn <alperdogan@hotmail.com>
2023-08-17 11:15:30 +0300

    Update inspecting-HTML.py

--- locators-in-webdriver/inspecting-HTML.py
+++ locators-in-webdriver/inspecting-HTML.py
@@ -17,8 +17,9 @@ driver.find_element(By.NAME, 'email').send_keys(EMAIL)
 driver.find_element(By.ID, 'exampleInputPassword1').send_keys(PASSWORD)
 driver.find_element(By.ID, 'exampleCheck1').click()
 
-# tagname[attribute='value'] custom CSS SELECTOR
+# tagname[attribute='value'] custom CSS SELECTOR; #id; .classname
 driver.find_element(By.CSS_SELECTOR, 'input[name="name"]').send_keys(NAME)
+driver.find_element(By.CSS_SELECTOR, '#inlineRadio1').click()
 # //tagname[@attribute='value'] custom XPATH
 driver.find_element(By.XPATH, '//input[@type="submit"]').click()  # Submit button
 message = driver.find_element(By.CLASS_NAME, 'alert-success').text  # Alert after submitting
@@ -27,9 +28,11 @@ print(message)
 # Validation
 assert 'Success' in message
 
+driver.find_element(By.XPATH, '(//input[@type="text"])[3]').send_keys(', middle name huseyin')
+driver.find_element(By.XPATH, '(//input[@type="text"])[3]').clear()
+
 # Close driver
 driver.close()
-
 
 
 
