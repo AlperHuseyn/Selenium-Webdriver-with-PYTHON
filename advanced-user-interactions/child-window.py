dc0074e69aa665504dcdf31f5380e44a59d5019d
AlperHuseyn <alperdogan@hotmail.com>
2023-08-21 10:49:56 +0300

    Update child-window.py

--- advanced-user-interactions/child-window.py
+++ advanced-user-interactions/child-window.py
@@ -1,11 +1,10 @@
 'Import necessary modules'
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 
-WEBSITE_URL = r'https://www.youtube.com'
+WEBSITE_URL = r'https://randomfox.ca/'
 
 service = Service()
 driver = webdriver.Chrome(service=service)
@@ -14,5 +13,10 @@ driver.maximize_window()
 
 driver.get(WEBSITE_URL)
 
-actions = ActionChains(driver)
-
+driver.find_element(By.LINK_TEXT, 'GitHub').click()
+driver.switch_to.window(driver.window_handles[-1])
+assert 'xinitrc-dev' == driver.find_element(By.LINK_TEXT, 'xinitrc-dev').text
+driver.close()
+driver.switch_to.window(driver.window_handles[0])
+assert 'Add more floof!' == driver.find_element(By.LINK_TEXT, 'Add more floof!').text
+driver.close()
