c32d0c44d158ac89de77a4b6ef11a02839e8885f
AlperHuseyn <alperdogan@hotmail.com>
2023-08-26 21:39:18 +0300

    Update utilities.py

--- e2e-selenium-python-framework/utilities.py
+++ e2e-selenium-python-framework/utilities.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 import pytest
 
@@ -5,11 +6,17 @@ import pytest
 @pytest.mark.usefixtures('setup')
 class BaseClass:
     def get_logs(self):
-        logger = logging.getLogger(__name__)
+        # Get name of the calling function for contextual logging.
+        logger_name = inspect.stack[1][3]
+        # Create a logger instance using the calling function's name.
+        logger = logging.getLogger(logger_name)
+        # Create a file handler to write logs to a file named 'logfile.log'.
         file_handler =logging.FileHandler('logfile.log')
         logger.addHandler(file_handler)
+        # Define a log message format with timestamp, log level, logger name, and message.
         formatter = logging.Formatter('%(asctime)s :%(levelname)s :%(name)s :%(message)s')
         file_handler.setFormatter(formatter)
+        # Set the logging level to INFO, meaning it will capture messages of INFO level and above.
         logger.setLevel(logging.INFO)
         
         return logger
