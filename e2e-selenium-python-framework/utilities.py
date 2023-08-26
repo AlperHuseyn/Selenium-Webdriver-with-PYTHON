0f88b715a90796d471f498ef5d4aaf8ac9c116dc
AlperHuseyn <alperdogan@hotmail.com>
2023-08-26 21:31:43 +0300

    Update utilities.py

--- e2e-selenium-python-framework/utilities.py
+++ e2e-selenium-python-framework/utilities.py
@@ -1,6 +1,15 @@
+import logging
 import pytest
 
 
 @pytest.mark.usefixtures('setup')
 class BaseClass:
-    pass
+    def get_logs(self):
+        logger = logging.getLogger(__name__)
+        file_handler =logging.FileHandler('logfile.log')
+        logger.addHandler(file_handler)
+        formatter = logging.Formatter('%(asctime)s :%(levelname)s :%(name)s :%(message)s')
+        file_handler.setFormatter(formatter)
+        logger.setLevel(logging.INFO)
+        
+        return logger
