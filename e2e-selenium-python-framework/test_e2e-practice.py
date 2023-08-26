d3b9e2acc07c056f7f3238b20b4245a2df72390b
AlperHuseyn <alperdogan@hotmail.com>
2023-08-26 20:45:17 +0300

    Correct confirmation object name and add a comment on line #20

--- e2e-selenium-python-framework/test_e2e-practice.py
+++ e2e-selenium-python-framework/test_e2e-practice.py
@@ -17,11 +17,12 @@ class TestE2EFramework(BaseClass):
         checkout.switch_to_checkout_page().click()
         checkout.checkout().click()
         
-        conformation = ConfirmationPage(self.driver)
-        conformation.enter_country()
-        conformation.wait_for_country_to_appear()
-        conformation.choose_country_in_the_list().click()
-        conformation.accept_agreement().click()
-        conformation.purchase().click()
+        # Do the things relted to confirmation
+        confirmation = ConfirmationPage(self.driver)
+        confirmation.enter_country()
+        confirmation.wait_for_country_to_appear()
+        confirmation.choose_country_in_the_list().click()
+        confirmation.accept_agreement().click()
+        confirmation.purchase().click()
 
         assert 'Success' in self.driver.find_element(By.CSS_SELECTOR, '.alert-success').text
