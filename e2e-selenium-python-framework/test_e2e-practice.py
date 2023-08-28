36058a26f80d29c1a22af20def7863f0783047f1
AlperHuseyn <alperdogan@hotmail.com>
2023-08-28 11:48:01 +0300

    Update test_e2e-practice.py

--- e2e-selenium-python-framework/test_e2e-practice.py
+++ e2e-selenium-python-framework/test_e2e-practice.py
@@ -1,6 +1,4 @@
 'Import necessary modules'
-from CheckoutPage import CheckoutPage
-from ConfirmationPage import ConfirmationPage
 from selenium.webdriver.common.by import By
 from utilities import BaseClass
 from HomePage import HomePage
@@ -9,16 +7,14 @@ from HomePage import HomePage
 class TestE2EFramework(BaseClass):
     def test_e2e_framework(self):
         # Switch to shop page
-        HomePage(self.driver).shop_button().click()
+        checkout = HomePage(self.driver).switch_to_checkout_page()
 
         # Do the things related to checkout
-        checkout = CheckoutPage(self.driver)
         checkout.add_to_card().click()
         checkout.switch_to_checkout_page().click()
-        checkout.checkout().click()
-        
+        confirmation = checkout.checkout()
+
         # Do the things relted to confirmation
-        confirmation = ConfirmationPage(self.driver)
         confirmation.enter_country()
         confirmation.wait_for_country_to_appear()
         confirmation.choose_country_in_the_list().click()
