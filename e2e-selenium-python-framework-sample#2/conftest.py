ebb9b636fbdbedb97cfb7d77c15e227de22b881a
AlperHuseyn <alperdogan@hotmail.com>
2023-08-28 20:56:50 +0300

    Update conftest.py

--- e2e-selenium-python-framework-sample#2/conftest.py
+++ e2e-selenium-python-framework-sample#2/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+driver = None
 
 
 WEBSITE_URL = r'https://rahulshettyacademy.com/angularpractice/'
@@ -11,6 +12,8 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='class')
 def setup(request):
+    global driver
+
     browser_name = request.config.getoption('--browser-name')
 
     if browser_name == 'chrome':
@@ -20,7 +23,7 @@ def setup(request):
     elif browser_name == 'edge':
         service = webdriver.EdgeService()
         driver = webdriver.Edge(service=service)
-        
+
     # Other browsers can be added as well.
 
     driver.get(WEBSITE_URL)
@@ -28,3 +31,29 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.close()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report,
+        whenever test fails.
+        :param item:
+    """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
